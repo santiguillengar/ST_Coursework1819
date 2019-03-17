076dfa7b4bca00cbe30da7b208748ba1a0d7c076
João Catarino <cata.catarino@gmail.com>
2019-03-17 00:05:58 +0000

    Task 1 Review need santi approval

--- SourceCode/src/Task1_Functional.java
+++ SourceCode/src/Task1_Functional.java
@@ -46,38 +46,46 @@ private Parser parser;
 	public void test_32_name_numeric_start() {
 		parser.add("1test", "t", Parser.STRING);
 	}
+	
 	@Test(expected = RuntimeException.class)
 	public void test_32_name_contains_asterisk() {
 		parser.add("test*", "t", Parser.STRING);
 	}
-	public void test_32_name_contains_numbers() {
-		parser.add("test1", "t", Parser.STRING);
-	}
-	public void test_32_name_contains_underscore() {
-		parser.add("test_", "t", Parser.STRING);
-	}
+	
 	@Test(expected = RuntimeException.class)
 	public void test_32_shortcut_numeric_start() {
 		parser.add("test", "1t", Parser.STRING);
 	}
+	
 	@Test(expected = RuntimeException.class)
 	public void test_32_shortcut_contains_asterisk() {
 		parser.add("test", "t*", Parser.STRING);
 	}
-	public void test_32_shortcut_contains_numbers() {
-		parser.add("test", "t1", Parser.STRING);
-	}
-	public void test_32_shortcut_contains_underscore() {
-		parser.add("test", "t_", Parser.STRING);
+	
+	@Test
+	public void test_32_name() {
+		// Letters
+		for(char ch = 'a'; ch <= 'z'; ++ch){
+			parser.add(Character.toString(ch), Character.toString(ch), Parser.STRING);
+        } 
+		for(char ch = 'A'; ch <= 'Z'; ++ch){
+			parser.add(Character.toString(ch), Character.toString(ch), Parser.STRING);
+        }
+		// Numbers and underscore
+		for(char ch = '0'; ch <= '9'; ++ch){
+			parser.add("a_" + ch, "a_" + ch, Parser.STRING);
+        } 
 	}
 	
 	@Test
 	public void test_33() {
 
 		parser.add("output", "o", Parser.STRING);
+		parser.add("Output", "O", Parser.INTEGER);
+
 		
 		parser.parse("--Output=output.txt");
-		assertEquals(parser.getString("Output"), "");
+		assertEquals(parser.getInteger("Output"), 0);
 		
 		parser.parse("--output=output.txt");
 		assertEquals(parser.getString("output"), "output.txt");
@@ -86,7 +94,7 @@ private Parser parser;
 		assertEquals(parser.getString("o"), "output.txt");
 		
 		parser.parse("-O=output.txt");
-		assertEquals(parser.getString("O"), "");
+		assertEquals(parser.getInteger("O"), 0);
 
 	}
 	
@@ -112,38 +120,48 @@ private Parser parser;
 	
 	@Test
 	public void test_35() {
-
+		
 		parser.add("output", "o", Parser.BOOLEAN);
+
 		
-		parser.parse("--output=0");
-		assertEquals(parser.getBoolean("output"), false);
+		String[] part1 = {"--output ", 
+						  "-o ",
+						  "--output=",
+						  "-o="};
+		String[] part2true = {"1",
+							  "100",
+							  "true",
+							  "True",
+							  "TRUE"};
+		String[] part2false = {"0",
+							   "false",
+							   "False",
+							   "FALSE"};
 		
-		parser.parse("--output=false");
-		assertEquals(parser.getBoolean("output"), false);
-		
-		parser.parse("--output=False");
-		assertEquals(parser.getBoolean("output"), false);
-		
-		parser.parse("--output=FALSE");
-		assertEquals(parser.getBoolean("output"), false);
-		
+		// Empty case
 		parser.parse("");
 		assertEquals(parser.getBoolean("output"), false);
 		
-		parser.parse("--output=1");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=true");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=True");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=TRUE");
-		assertEquals(parser.getBoolean("output"), true);
-		
+		// Empty definition case
 		parser.parse("--output");
 		assertEquals(parser.getBoolean("output"), true);
+		parser.parse("-o");
+		assertEquals(parser.getBoolean("output"), true);
+		
+		//Matrix of tests
+		for (String firstpart : part1) {
+			for (String secondpart : part2false) {
+				parser.parse(firstpart + secondpart);
+				assertEquals(parser.getBoolean("output"), false);
+			}
+		}
+		
+		for (String firstpart : part1) {
+			for (String secondpart : part2true) {
+				parser.parse(firstpart + secondpart);
+				assertEquals(parser.getBoolean("output"), true);
+			}
+		}
 	}
 	
 	
@@ -164,24 +182,36 @@ private Parser parser;
 	public void test_42_name_numeric_start() {
 		parser.add("1test", Parser.STRING);
 	}
+	
 	@Test(expected = RuntimeException.class)
 	public void test_42_name_contains_asterisk() {
 		parser.add("test*", Parser.STRING);
 	}
-	public void test_42_name_contains_numbers() {
-		parser.add("test1", Parser.STRING);
+	
+	@Test
+	public void test_42_name() {
+		// Letters
+		for(char ch = 'a'; ch <= 'z'; ++ch){
+			parser.add(Character.toString(ch), Parser.STRING);
+        } 
+		for(char ch = 'A'; ch <= 'Z'; ++ch){
+			parser.add(Character.toString(ch), Parser.STRING);
+        }
+		// Numbers and underscore
+		for(char ch = '0'; ch <= '9'; ++ch){
+			parser.add("a_" + ch, Parser.STRING);
+        } 
 	}
-	public void test_42_name_contains_underscore() {
-		parser.add("test_", Parser.STRING);
-	}
-
+	
 	@Test
 	public void test_43() {
 
 		parser.add("output", Parser.STRING);
+		parser.add("Output", Parser.INTEGER);
+
 		
-		parser.parse("--Output=output.txt");
-		assertEquals(parser.getString("Output"), "");
+		parser.parse("--Output=1");
+		assertEquals(parser.getInteger("Output"), 1);
 		
 		parser.parse("--output=output.txt");
 		assertEquals(parser.getString("output"), "output.txt");
@@ -193,35 +223,44 @@ private Parser parser;
 
 		parser.add("output", Parser.BOOLEAN);
 		
-		parser.parse("--output=0");
-		assertEquals(parser.getBoolean("output"), false);
+		String[] part1 = {"--output ", 
+						  "-o ",
+						  "--output=",
+						  "-o="};
+		String[] part2true = {"1",
+							  "100",
+							  "true",
+							  "True",
+							  "TRUE"};
+		String[] part2false = {"0",
+							   "false",
+							   "False",
+							   "FALSE"};
 		
-		parser.parse("--output=false");
-		assertEquals(parser.getBoolean("output"), false);
-		
-		parser.parse("--output=False");
-		assertEquals(parser.getBoolean("output"), false);
-		
-		parser.parse("--output=FALSE");
-		assertEquals(parser.getBoolean("output"), false);
-		
+		// Empty case
 		parser.parse("");
 		assertEquals(parser.getBoolean("output"), false);
 		
-		parser.parse("--output=1");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=true");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=True");
-		assertEquals(parser.getBoolean("output"), true);
-		
-		parser.parse("--output=TRUE");
-		assertEquals(parser.getBoolean("output"), true);
-		
+		// Empty definition case
 		parser.parse("--output");
 		assertEquals(parser.getBoolean("output"), true);
+		parser.parse("-o");
+		assertEquals(parser.getBoolean("output"), true);
+		
+		//Matrix of tests
+		for (String firstpart : part1) {
+			for (String secondpart : part2false) {
+				parser.parse(firstpart + secondpart);
+				assertEquals(parser.getBoolean("output"), false);
+			}
+		}
+		
+		for (String firstpart : part1) {
+			for (String secondpart : part2true) {
+				parser.parse(firstpart + secondpart);
+				assertEquals(parser.getBoolean("output"), true);
+			}
+		}
 	}
 
 	
@@ -232,7 +271,6 @@ private Parser parser;
 		parser.add("output", "o", Parser.STRING);
 		assertEquals(parser.parse("--output=output.txt"), 0);
 		
-		parser.parse("--output=output.txt");
 		assertEquals(parser.getString("output"), "output.txt");
 	}
 	
@@ -241,7 +279,6 @@ private Parser parser;
 		parser.add("output", "o", Parser.STRING);
 		assertEquals(parser.parse("-o=output.txt"), 0);
 		
-		parser.parse("-o=output.txt");
 		assertEquals(parser.getString("output"), "output.txt");
 	}
 	
@@ -259,17 +296,24 @@ private Parser parser;
 	
 	@Test
 	public void test_54() {
-
+		
 		parser.add("output", "o", Parser.STRING);
+
+		String[] part1 = {"--output ", 
+						  "-o ",
+						  "--output=",
+						  "-o="};
+		String[] part2 = {"value",
+						  "\"value\"",
+						  "'value'"};
 		
-		parser.parse("--output=\"output.txt\"");
-		assertEquals(parser.getString("output"), "output.txt");
-		
-		parser.parse("--output='output2.txt'");
-		assertEquals(parser.getString("output"), "output2.txt");
-		
-		parser.parse("--output=output3.txt");
-		assertEquals(parser.getString("output"), "output3.txt");
+		//Matrix of tests
+		for (String firstpart : part1) {
+			for (String secondpart : part2) {
+				parser.parse(firstpart + secondpart);
+				assertEquals(parser.getString("output"), "value");
+			}
+		}
 	}
 	
 	@Test
